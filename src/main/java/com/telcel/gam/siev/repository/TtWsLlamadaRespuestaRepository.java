805a1c461b3814909d7b6102a38b9528641c781d
Juan Ignacio Castro Flores <castroflores.juan@yahoo.com>
2019-05-24 14:03:53 -0500

    Se sube la llamada/respuesta

---
+++ src/main/java/com/telcel/gam/siev/repository/TtWsLlamadaRespuestaRepository.java
@@ -0,0 +1,9 @@
+package com.telcel.gam.siev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.telcel.gam.siev.modelo.TtWsLlamadaRespuesta;
+
+public interface TtWsLlamadaRespuestaRepository extends JpaRepository<TtWsLlamadaRespuesta, Integer> {
+
+}
